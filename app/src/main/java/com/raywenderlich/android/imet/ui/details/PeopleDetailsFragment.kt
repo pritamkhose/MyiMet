c264d3ffb73b0d3fe86f64688a4c96eda51ac970
Pritam <pritamkhose@gmail.com>
2020-07-17 21:46:07 +0530

    add people, details fragment

--- app/src/main/java/com/raywenderlich/android/imet/ui/details/PeopleDetailsFragment.kt
+++ app/src/main/java/com/raywenderlich/android/imet/ui/details/PeopleDetailsFragment.kt
@@ -6,15 +6,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
 import com.raywenderlich.android.imet.R
+import com.raywenderlich.android.imet.data.model.People
+import kotlinx.android.synthetic.main.people_details_fragment.*
 
 class PeopleDetailsFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = PeopleDetailsFragment()
-    }
-
     private lateinit var viewModel: PeopleDetailsViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        viewModel = ViewModelProviders.of(this).get(PeopleDetailsViewModel::class.java)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,10 +27,28 @@ class PeopleDetailsFragment : Fragment() {
         return inflater.inflate(R.layout.people_details_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(PeopleDetailsViewModel::class.java)
-        // TODO: Use the ViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Find people with provided id
+        val peopleId = arguments?.getInt(getString(R.string.people_id))
+        peopleId?.let {
+            viewModel.getPeopleDetails(peopleId).observe(viewLifecycleOwner, Observer { peopleDetails ->
+                populatePeopleDetails(peopleDetails)
+            })
+        }
+    }
+
+    /**
+     * Binds people info into views
+     */
+    private fun populatePeopleDetails(people: People?) {
+        textViewName.text = people?.name
+        textViewMet.text = people?.metAt
+        buttonContact.text = people?.contact
+        textViewEmail.text = people?.email
+        textViewFacebook.text = people?.facebook
+        textViewTwitter.text = people?.twitter
     }
 
 }
